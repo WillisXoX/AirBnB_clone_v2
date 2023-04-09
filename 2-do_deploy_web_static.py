88e82c559e73343cc52468593598aa3b5506f48b
WillisXoX <williamstone4041@gmail.com>
2023-04-09 15:27:05 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ Usage:
 """
 from fabric.api import env, put, run
 import os.path
-env.hosts = ['35.229.54.225', '35.231.225.251']
+env.hosts = ['34.205.65.100', '35.175.134.25	']
 
 
 def do_deploy(archive_path):
